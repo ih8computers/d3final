9abd203ff59f464cfb3db92f34ae0d90ce17a9af
ih8computers <ih8computers>
2020-08-02 12:28:03 -0400

    age initial draw grouped successfully

--- src/age.js
+++ src/age.js
@@ -9,7 +9,6 @@ function age_chart(){
   var scene = scene_list[slide_index];
   console.log(slide_index);
   var fdata = getFilteredData(scene.data, filters);
-
   yearIndex = 0;
   fdata = fdata.filter(function(d){ return d.MaritalStatus == filters.status;});
   years = getYears(fdata);
@@ -21,33 +20,64 @@ function age_chart(){
   x=d3.scaleBand().domain(domain).range([0,width]).padding(0.2);
   y=d3.scaleLinear().domain([0,100]).range([height,0]);
 
-  d3.select('svg').attr('width',width+2*margin)
-                  .attr('height', height+2*margin)
-                  .append('g')
-                  .classed('rect-group', true)
-                  .attr('transform',"translate("+margin+","+margin+")")
-                  .selectAll('rect').data(fdata).enter().append('rect')
-                  .attr('x', function(d,i){return x(d.AgeGroup);})
-                  .attr('y', function(d,i){return y(d.DataValue);})
-                  .attr('width', function(d,i){return x.bandwidth()})
-                  .attr('height', function(d,i){return height-y(d.DataValue);});
+  var groups = ['Men', 'Women'];
+  var xgroupScale = d3.scaleBand()
+  .domain(groups)
+  .range([0, x.bandwidth()])
+  .padding([0.05]);
 
-  d3.select('svg').attr('width',width+2*margin)
-                  .attr('height', height+2*margin)
-                  .append('g')
-                  .classed('xaxis-group',true)
-                  .attr('transform',"translate("+margin+","+xax+")")
-                  .call(d3.axisBottom(x));
-  d3.select('svg').attr('width',width+2*margin)
-                  .attr('height', height+2*margin)
-                  .append('g')
-                  .classed('yaxis-group',true)
-                  .attr('transform',"translate("+margin+","+margin+")")
-                  .attr('height',height)
-                  .call(d3.axisLeft(y));
+  var color = d3.scaleOrdinal()
+    .domain(groups)
+    .range(['lightblue','pink'])
+
+  var yvals = [];
+  var sexMax = [];
+  sexMax["Men"] = {max : 0, age : -13};
+  sexMax["Women"] = {max : 0, age : -13};
+
+  var bars = d3.select('svg')
+              .append('g')
+              .attr('transform',"translate("+margin+","+margin+")")
+              .classed('over',true)
+              .selectAll("g")
+              .data(fdata)
+              .join("g")
+                .attr("transform", d => `translate(${x(d['AgeGroup'])},0)`)
+              .selectAll("rect")
+              .data(function(d){
+
+                ret = groups.map(key => ({key: key, value: d.Sex == key ? d.DataValue : 0}));
+                console.log(d.Sex);
+                if(sexMax[d.Sex].max < d.DataValue){
+                  sexMax[d.Sex].max = parseFloat(d.DataValue);
+                  sexMax[d.Sex].age = parseFloat(d.AgeGroup);
+                }
+                return ret;
+              })
+              //.data(function(d){ret = {key:d.Sex, value:d.DataValue}; console.log(ret); return ret;})
+              .join('rect')
+                .attr("x", function(d){ return xgroupScale(d.key);})
+                .attr("y", function(d){ console.log(d.value);return y(d.value);})
+                .attr("width", xgroupScale.bandwidth())
+                .attr("height", d => height - y(d.value))
+                .attr("fill", d => color(d.key));
+
+d3.select('svg').attr('width',width+2*margin)
+                .attr('height', height+2*margin)
+                .append('g')
+                .classed('x-axisgroup', true)
+                .attr('transform',"translate("+margin+","+xax+")")
+                .call(d3.axisBottom(x));
+d3.select('svg').attr('width',width+2*margin)
+                .attr('height', height+2*margin)
+                .append('g')
+                .classed('y-axisgroup', true)
+                .attr('transform',"translate("+margin+","+margin+")")
+                .attr('height',height)
+                .call(d3.axisLeft(y));
 
 
-  setTimeout(function(){startAnimation();},5000);
+  //setTimeout(function(){startAnimation();},5000);
   //interval_id = window.setInterval(cycleYears, 1000)
 }
 
@@ -56,7 +86,6 @@ function updateAgeChart(){
   var scene = scene_list[slide_index];
   console.log(slide_index);
   var fdata = getFilteredData(scene.data, filters);
-
   fdata = fdata.filter(function(d){ return d.MaritalStatus == filters.status;});
   years = getYears(fdata);
   fdata = fdata.filter(function(d){ return d.YearStart == years[yearIndex];});
@@ -83,6 +112,7 @@ function updateAgeChart(){
                 	.attr('transform',"translate("+margin+","+margin+")")
                 	.attr('height',height)
                 	.call(d3.axisLeft(y));//.tickValues([0,100]).tickFormat(d3.format("~s")));
+
 }
 
 function getYears(cdata){
